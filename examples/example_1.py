b174ca280afd0b71f6a3c2e2a37d32f61503f3f3
Kirilin Artem <kirilinartem@yandex.ru>
2025-02-27 23:16:25 +0700

    Примеры

---
+++ examples/example_1.py
@@ -0,0 +1,35 @@
+import numpy as np
+from polyominator import polyomino
+
+area = np.ones((6,4))
+
+figures = [
+    np.array([
+        [1,1,1,1]
+    ])
+    ,np.array([
+        [1,1]
+        ,[1,1]
+    ])
+    ,np.array([
+        [1,1,1]
+        ,[0,1,0]
+    ])
+    ,np.array([
+        [1,1,1]
+        ,[0,1,0]
+    ])
+    ,np.array([
+        [1,1,1]
+        ,[1,0,0]
+    ])
+    ,np.array([
+        [1,1,0]
+        ,[0,1,1]
+    ])
+]
+
+
+pm = polyomino(figures,area)
+solutions = pm.solve()
+print(solutions[0])
